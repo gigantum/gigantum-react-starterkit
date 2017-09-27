5e96d6382915417cf7bd68afb9854cf954633f53
Kay Ehni <kay.ehnigmail.com>
2017-09-27 15:39:11 -0400

    added title

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render () {
     return (
         <div className='root'>
-          <Header />
+          <Header title="Should I bike?"/>
           <Weather />
         </div>
 
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -10,7 +10,6 @@ export default class Header extends Component {
     return (
       <div className='app-header relative'>
         <h1 className='app-title text-center'>{title}</h1>
-        <img className="app-logo" src={logoImg} alt="app-logo" />
       </div>
     );
   }
