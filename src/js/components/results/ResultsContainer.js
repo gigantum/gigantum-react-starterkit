b675cfa0df493f1a9c32c52eb56ed4d26e5b143f
Kay Ehni <kay.ehnigmail.com>
2017-09-27 14:53:42 -0400

    added results/results container components, plain and green bike and metro images

---
+++ src/js/components/results/ResultsContainer.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import Results from 'js/components/Results'
+
+import {weatherStore} from 'js/components/weather/WeatherStore';
+import {illuminatePic} from 'js/utils/illuminate-logo.js'
+
+export default class ResultsContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.bikePhoto = null;
+    this.metroPhoto = null;
+    this.state = weatherStore.getState();
+    weatherStore.listen(this.storeDidUpdate.bind(this));
+  }
+
+  storeDidUpdate = () => {
+    this.setState(weatherStore.getState());
+  }
+
+  render () {
+    let caveat = null;
+    if(this.state.hourly===false){ //shows caveat if hourly data is not available
+      caveat = "*Data based on daily forecast. Check back 48 hours before your commute for a more accurate prediction"
+    }
+    const pics = illuminatePic()
+    return (
+        <Results bikeClass = "bike-logo" metroClass = "metro-logo" transport={this.state.transport}
+        bikeSrc={pics.bikePhoto} metroSrc={pics.metroPhoto} caveat={caveat}/>
+    )
+  }
+}
