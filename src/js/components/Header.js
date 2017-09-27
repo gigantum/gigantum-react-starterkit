f81ced06fb92ef04aa6a00ff6ff2b972b6c6a071
Kay Ehni <kay.ehnigmail.com>
2017-09-27 15:41:04 -0400

    added commute time component to header

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import logoImg from 'images/bicycle.svg';
+import CommuteTime from 'js/components/CommuteTime';
 
 export default class Header extends Component {
   displayName: 'Header';
@@ -10,6 +11,7 @@ export default class Header extends Component {
     return (
       <div className='app-header relative'>
         <h1 className='app-title text-center'>{title}</h1>
+        <CommuteTime/>
       </div>
     );
   }
