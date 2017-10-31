0d82a1c6fcfa345dd7f108a9fe96856a6605c713
August Conwell <august.conwell@gmail.com>
2017-10-30 23:36:49 -0400

    Get location and weather data on component mount

--- src/js/components/weather/Weather.js
+++ src/js/components/weather/Weather.js
@@ -1,17 +1,17 @@
 //Vendor
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 //Locals
-import {weatherStore} from 'js/components/weather/WeatherStore';
-import {weatherActions} from 'js/components/weather/WeatherActions';
+import { weatherStore } from 'js/components/weather/WeatherStore';
+import { weatherActions } from 'js/components/weather/WeatherActions';
 
-import {viewOptions} from 'js/config';
+import { viewOptions } from 'js/config';
 
 export default class Body extends Component {
-  constructor(props){
-		super(props);
-		this.state = weatherStore.getState();
-		weatherStore.listen(this.storeDidUpdate.bind(this));
-	}
+  constructor(props) {
+    super(props);
+    this.state = weatherStore.getState();
+    weatherStore.listen(this.storeDidUpdate.bind(this));
+  }
 
   /*
     get api data when the component mounts
@@ -19,13 +19,21 @@ export default class Body extends Component {
   componentDidMount() {
     // Subscribe to the store for updates
     weatherActions.initialize(); //initialize store
+    weatherStore.updateLocation()
+      .then(weatherStore.updateWeather)
+      // .then((response) => {
+      //   console.log('weather', response)
+      // })
+      .catch((error) => {
+        console.error('error', error)
+      })
   }
 
   storeDidUpdate = () => {
     this.setState(weatherStore.getState());//triggers re-render when store updates
   }
 
-  render () {
+  render() {
 
     return (
       <div>App</div>
