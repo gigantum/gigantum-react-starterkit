e83eb66842df2b7606a36a6aea157b5176606ff7
August Conwell <august.conwell@gmail.com>
2017-10-31 00:10:55 -0400

    Natural file tree

--- src/js/components/weather/range-selector/RangeSelector.js
+++ src/js/components/weather/range-selector/RangeSelector.js
@@ -1,14 +1,11 @@
 //Vendor
 import React, {Component} from 'react';
-//Locals
-import {weatherStore} from 'js/components/weather/WeatherStore';
-import {weatherActions} from 'js/components/weather/WeatherActions';
 
 // A customizeable slider 
 const RangeSelector = (props) => {
   return (
     <div>
-      Testing
+      <input type="range" min={props.min} max={props.max} onChange={props.handleChange} value={props.value}/>
     </div>
   )
 }
