dc7021a5327b042448c481210692f48ba7112dc8
August Conwell <august.conwell@gmail.com>
2017-10-31 23:30:34 -0400

    Prevented flashing grab cursor

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,18 +1,30 @@
 //Vendor
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 //Components
 import Header from 'js/components/Header';
 import Weather from 'js/components/weather/Weather';
+import {weatherStore} from 'js/components/weather/WeatherStore'
 
 export default class App extends Component {
-  displayName: 'App';
+  constructor(props) {
+    super(props);
+    this.state = weatherStore.getState();
+    weatherStore.listen(this.storeDidUpdate.bind(this));
+  }
 
-  render () {
+  storeDidUpdate = () => {
+    if (this.state.isGrabbing !== weatherStore.getState().isGrabbing) {
+      this.setState({ isGrabbing: weatherStore.getState().isGrabbing });//triggers re-render when store updates
+    }
+  }
+
+  render() {
+    // include grabbing or not-grabbing class to stop flashing cursor when moving sliders
     return (
-        <div className='root'>
-          <Header />
-          <Weather />
-        </div>
+      <div className={`root ${this.state.isGrabbing}`}>
+        <Header />
+        <Weather />
+      </div>
 
     );
   }
