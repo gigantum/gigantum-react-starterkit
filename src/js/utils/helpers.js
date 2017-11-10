032f6f7fa9d0a6fbb7300f09d89be5644e4efc15
August Conwell <august.conwell@gmail.com>
2017-11-09 21:13:27 -0500

    added commuteWeather function to return weather at specific time

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -13,20 +13,33 @@ export const hoursTillCommute = (commuteTime, now) => {
     return commuteTime + 24 - hoursNow;
 }
 
+export const commuteWeather = (weather, commuteHour) => {
+    const hourOfCommute = hoursTillCommute(commuteHour, new Date())
+    return weather.hourly.data[hourOfCommute]
+}
+
 /**
  * Determines if the weather meets the user preferences for biking
  * @return {boolean}
  */
-export const youShouldBikeToday = ({weather, morningCommute, temperatureRange, maxChanceOfRain }) => {
-    const hourOfCommute = hoursTillCommute(morningCommute, new Date())
-    const { temperature, precipProbability } = weather.hourly.data[hourOfCommute];
-    if (
-        temperature >= temperatureRange[0] &&
-        temperature <= temperatureRange[1] &&
-        precipProbability * 100 <= maxChanceOfRain
-    ) {
-        return true
+export const youShouldBikeToday = ({weather, morningCommute, eveningCommute, temperatureRange, maxChanceOfRain }) => {
+    const youShouldBikeIn = ({ temperature, precipProbability }, temperatureRange, maxChanceOfRain) => {
+        if (
+            temperature >= temperatureRange[0] &&
+            temperature <= temperatureRange[1] &&
+            precipProbability * 100 <= maxChanceOfRain
+        ) {
+            return true
+        }
+        return false
     }
-    return false
+
+    const today = new Date();
+    const hourOfMorningCommute = hoursTillCommute(morningCommute, today)
+    const hourOfEveningCommute = hoursTillCommute(eveningCommute, today)
+    const morningWeather = weather.hourly.data[hourOfMorningCommute];
+    const eveningWeather = weather.hourly.data[hourOfEveningCommute];
+
+    return youShouldBikeIn(morningWeather, temperatureRange, maxChanceOfRain) && youShouldBikeIn(eveningWeather, temperatureRange, maxChanceOfRain); 
 }
 
